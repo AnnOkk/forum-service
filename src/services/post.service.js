a7e25e39d95b9cf772fcb16ce0dcfed9353bf51f
AnnOkk <chereshnyasladkaya@gmail.com>
2026-03-25 15:49:23 +0200

    Implement basic post features: controllers, routes, and error handling

---
+++ src/services/post.service.js
@@ -0,0 +1,13 @@
+class PostService {
+    async createPost(author,data){
+        //TODO addPost data {}
+        throw new Error('Not implemented');
+    }
+    async getPostById(id){
+        //TODO getPostById id
+        throw new Error('Not implemented');
+    }
+
+
+}
+export default new PostService;
