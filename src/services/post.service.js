8dc00afa3f1ef109e9a7835123d909f6dd3c0a26
edward <edwardlfish@gmail.com>
2026-03-27 11:00:48 +0300

    Add Post and Comment models, repository, and related service methods

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,15 +1,50 @@
+import postRepository from "../repositories/post.repository.js";
+
 class PostService {
     async createPost(author, data) {
-        // TODO add post. data example: {
-        // 	"title": "JavaEE",
-        // 	"content": "Java is the best for backend",
-        // 	"tags":["Java", "Spring", "backend", "JEE"]
-        // }
-        throw new Error('Not implemented');
+        return await postRepository.createPost({author, ...data});
     }
 
     async getPostById(id) {
-        // TODO get post by id
+        const post = await postRepository.findPostById(id);
+        if (!post) {
+            throw new Error(`Post with id = ${id} not found`);
+        }
+        return post;
+    }
+
+    async deletePost(id) {
+        // TODO delete post by id
+        throw new Error('Not implemented');
+    }
+
+    async addLike(id) {
+        // TODO add like to post by id
+        throw new Error('Not implemented');
+    }
+
+    async getPostsByAuthor(author) {
+        // TODO get posts by author
+        throw new Error('Not implemented');
+    }
+
+    async addComment(id, commenter, content) {
+        // TODO add comment to post by id
+        throw new Error('Not implemented');
+    }
+
+    async getPostsByTags(tagsString) {
+        // TODO get posts by tags
+        throw new Error('Not implemented');
+    }
+
+    async getPostsByPeriod(dateFrom, dateTo) {
+        // TODO get posts by period
+        throw new Error('Not implemented');
+    }
+
+    async updatePost(id, data) {
+        // TODO update post by id
         throw new Error('Not implemented');
     }
 }
