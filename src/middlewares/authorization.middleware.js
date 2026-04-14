df7bb541d3b204d31d90612c4e8a9dcd530e81b2
edward <edwardlfish@gmail.com>
2026-04-14 21:08:32 +0300

    Add admin initialization, role-based authorization middleware, and integrate admin-only role management routes

---
+++ src/middlewares/authorization.middleware.js
@@ -0,0 +1,8 @@
+class Authorization {
+    hasRole(role) {
+        console.log(role)
+        return (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+    }
+}
+
+export default new Authorization();
