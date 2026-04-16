4a898eda25569951c1ad1ce04ce4affb8f7ba3ea
AnnOkk <chereshnyasladkaya@gmail.com>
2026-04-16 12:47:32 +0300

    Refactor role constants and enhance authorization middleware logging

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,8 +1,14 @@
 class Authorization {
     hasRole(role) {
-        console.log(role)
-        return (req, res, next) => req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+
+        return (req, res, next) => {
+            console.log('req.principal:', req.principal);
+            console.log('roles:', req.principal.roles);
+            console.log('required:', role.toUpperCase().trim());
+            console.log(role)
+        req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+
     }
-}
+}}
 
 export default new Authorization();
