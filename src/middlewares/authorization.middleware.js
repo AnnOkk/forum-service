7744a8db8c593d0aba6366c8b9410223085d1955
AnnOkk <chereshnyasladkaya@gmail.com>
2026-04-16 14:25:54 +0300

    Add `authPost` middleware and enhance authorization checks in routes

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,14 +1,56 @@
+import {ADMIN} from "../configuration/constants.js";
+
 class Authorization {
-    hasRole(role) {
-
+    isOwner() {
         return (req, res, next) => {
-            console.log('req.principal:', req.principal);
-            console.log('roles:', req.principal.roles);
-            console.log('required:', role.toUpperCase().trim());
-            console.log(role)
-        req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
-
+            console.log('principal:', req.principal.userName);
+            console.log('params.login:', req.params.login);
+            if (!req.principal) {
+                return res.status(401).json({message: 'Authorization required'});
+            }
+            if (req.principal.userName !== req.params.login) {
+                return res.status(403).json({message: 'Access denied'});
+            }
+            return next();
+        }
     }
-}}
+
+    isOwnerOrAdmin() {
+        return (req, res, next) => {
+            if (!req.principal) {
+                return res.status(401).json({message: 'Authorization required'});
+            }
+            const isOwner = req.principal.userName === req.params.login;
+            const isAdmin = req.principal.roles.includes(ADMIN);
+
+            if (!isOwner && !isAdmin) {
+                return res.status(403).json({message: 'Access denied'});
+            }
+            return next();
+        }
+    }
+
+
+//     hasRole(role) {
+//         return (req, res, next) => {
+//             req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+//
+//         }
+//     }
+// }
+    hasRole(role) {
+        return (req, res, next) => {
+            if (!req.principal) {
+                return res.status(401).json({ message: 'Authorization required' });
+            }
+
+            if (!req.principal.roles.includes(role.toUpperCase().trim())) {
+                return res.status(403).json({ message: 'Access denied' });
+            }
+
+            return next();
+        };
+    }}
+
 
 export default new Authorization();
--- src/routes/userAccount.routes.js
+++ src/routes/userAccount.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import userAccountController from "../controllers/userAccount.controller.js";
 import validate from "../middlewares/validation.middleware.js";
+import authorization from "../middlewares/authorization.middleware.js";
 
 const router = Router();
 
 router.post("/register",validate('register'), userAccountController.register);
 router.post("/login", userAccountController.login);
-router.delete("/user/:login", userAccountController.deleteUser);
-router.patch("/user/:login",validate('updateUser'), userAccountController.updateUser);
+router.delete("/user/:login",authorization.isOwnerOrAdmin(), userAccountController.deleteUser);
+router.patch("/user/:login",authorization.isOwner(),validate('updateUser'), userAccountController.updateUser);
 router.patch("/user/:login/role/:role",validate('changeRoles', 'params'), userAccountController.addRole);
 router.delete("/user/:login/role/:role",validate('changeRoles', 'params'), userAccountController.deleteRole);
 router.patch("/password", userAccountController.changePassword);
